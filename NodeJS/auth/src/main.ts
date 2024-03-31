41776533bcb4ebcbd0751b2f40e076b84bfb5b23
John Rey Baylen <john.rey.baylen.pro@gmail.com>
2024-03-31 23:57:59 +0800

    chore: initial commit of auth service

---
+++ NodeJS/auth/src/main.ts
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv'
+import express from 'express'
+
+import { Logger } from '@utils/Logger'
+import { NODE_ENV } from '@lib/constants'
+
+const app = express()
+const logger = new Logger()
+const path = `.env${ NODE_ENV === 'development' ? '.local' : '' }`
+
+try {
+    dotenv.config({ path })
+    app.listen(
+        process.env.PORT,
+        function() {
+            logger.info(`Auth service is now running at ${ process.env.HOST }:${ process.env.PORT }`)
+        }
+    )
+} catch (error) {
+    logger.error(error as object)
+}
