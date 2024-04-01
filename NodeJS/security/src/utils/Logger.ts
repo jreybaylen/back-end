f817cbd33ba82429dbeb7207300b947fbbf62961
John Rey Baylen <john.rey.baylen.pro@gmail.com>
2024-04-02 02:36:14 +0800

    nodejs: (chore) add middleware and update tsconfig

--- NodeJS/security/src/utils/Logger.ts
+++ NodeJS/security/src/utils/Logger.ts
@@ -1,17 +1,41 @@
 import winston from 'winston'
 import dateFNS from 'date-fns'
 
+import { version } from '@root/package.json'
+
 export class Logger {
 	
 	#timestamp = dateFNS.format(new Date(), 'MM-dd-yyyy')
 
 	#logger = winston.createLogger({
-		format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+		format: winston.format.combine(
+			winston.format.timestamp(),
+			winston.format.json()
+		),
+		defaultMeta: {
+			service: 'security',
+			buildInfo: {
+				version: {
+					service: version,
+					node: process.version
+				}
+			}
+		},
 		transports: [
-			new winston.transports.Console(),
+			new winston.transports.Console({
+				format: winston.format.combine(
+					winston.format.colorize(),
+					winston.format.prettyPrint()
+				)
+			}),
 			new winston.transports.File({
 				handleExceptions: true,
-				filename: `logs/${ this.#timestamp }/log.txt`
+				filename: `logs/${ this.#timestamp }/combine.log`
+			}),
+			new winston.transports.File({
+				handleExceptions: true,
+				level: 'error',
+				filename: `logs/${ this.#timestamp }/error.log`
 			})
 		]
 	})
