d0291581cbe6b301547b507b7e6286a65f53b9dc
John Rey Baylen <john.rey.baylen.pro@gmail.com>
2025-06-07 18:04:50 +0800

    go: add method restriction

--- Go/tutorial-app/main.go
+++ Go/tutorial-app/main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
 
+func createPaymentIntent(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
+	http.HandleFunc("/create-payment-intent", createPaymentIntent)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		w.Write([]byte("Hello, World!"))
 	})
 
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Service is up!")
+	http.HandleFunc("/health", func(w http.ResponseWriter, req *http.Request) {
+		w.Write([]byte("Service is up!"))
 	})
 
+	log.Println("Server is running on port 4242")
 	if error := http.ListenAndServe(":4242", nil); error != nil {
 		log.Fatal(error)
 	}
