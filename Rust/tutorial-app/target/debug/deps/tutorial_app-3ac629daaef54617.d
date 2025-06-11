ca5e6f8bebe478586cb1f247a60b91130beb1170
John Rey Baylen <john.rey.baylen.pro@gmail.com>
2025-06-11 17:47:24 +0800

    rust: add .gitignore

--- Rust/tutorial-app/target/debug/deps/tutorial_app-3ac629daaef54617.d
+++
@@ -1,5 +0,0 @@
-/Users/jreybaylen/Desktop/projects/Hydra/back-end/Rust/tutorial-app/target/debug/deps/tutorial_app-3ac629daaef54617: src/main.rs
-
-/Users/jreybaylen/Desktop/projects/Hydra/back-end/Rust/tutorial-app/target/debug/deps/tutorial_app-3ac629daaef54617.d: src/main.rs
-
-src/main.rs:
--- Rust/tutorial-app/target/debug/tutorial-app.d
+++
@@ -1,1 +0,0 @@
-/Users/jreybaylen/Desktop/projects/Hydra/back-end/Rust/tutorial-app/target/debug/tutorial-app: /Users/jreybaylen/Desktop/projects/Hydra/back-end/Rust/tutorial-app/src/main.rs
