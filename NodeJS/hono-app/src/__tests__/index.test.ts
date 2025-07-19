f1775401ce4b0086d41319588d4280b1e1815f59
John Rey Baylen <john.rey.baylen.pro@gmail.com>
2025-07-19 19:49:52 +0800

    nodejs: add hono app with pm2

---
+++ NodeJS/hono-app/src/__tests__/index.test.ts
@@ -0,0 +1,8 @@
+import assert from 'node:assert/strict'
+import { describe, it } from 'node:test'
+
+describe('Test Sample', () => {
+  it('should run without error', () => {
+    assert.equal(1, 1)
+  })
+})
